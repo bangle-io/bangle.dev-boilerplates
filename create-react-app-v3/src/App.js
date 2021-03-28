7ea2f0dd684ee7ead410d455a37acccd2bfb54c5
Kushan Joshi <0o3ko0@gmail.com>
2021-03-27 17:17:20 -0700

    cra v3 and v4

--- create-react-app-v3/src/App.js
+++ create-react-app-v3/src/App.js
@@ -1,34 +1,6 @@
 import "./App.css";
-
-import "@bangle.dev/core/style.css";
-import "@bangle.dev/tooltip/style.css";
-import "@bangle.dev/react-menu/style.css";
-
-import { useEditorState, BangleEditor } from "@bangle.dev/react";
-import { corePlugins, coreSpec } from "@bangle.dev/core/utils/core-components";
-import { PluginKey } from "@bangle.dev/core";
-import { floatingMenu, FloatingMenu } from "@bangle.dev/react-menu";
-
-const menuKey = new PluginKey("menuKey");
-
-function Editor() {
-  const editorState = useEditorState({
-    initialValue: "Hello world!",
-    specs: coreSpec(),
-    plugins: () => [
-      ...corePlugins(),
-      floatingMenu.plugins({
-        key: menuKey,
-      }),
-    ],
-  });
-
-  return (
-    <BangleEditor state={editorState} style={{ padding: "5px 20px" }}>
-      <FloatingMenu menuKey={menuKey} />
-    </BangleEditor>
-  );
-}
+import React from 'react';
+import { Editor } from "./Editor";
 
 function App() {
   return (
