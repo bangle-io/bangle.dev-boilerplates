17e56720730f7a1452b9112508f019613a8330dc
Kushan Joshi <0o3ko0@gmail.com>
2021-07-20 17:02:51 -0700

    add a typescript example

---
+++ typescript__create-react-app-v4/src/index.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Editor } from "./Editor";
+
+ReactDOM.render(
+  <div className="App">
+    <Editor />
+  </div>,
+  document.getElementById("root")
+);
